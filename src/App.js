c19e6d8aa6b0eabd4f6a56ad6abc531217d715b6
Matt Wright <mattwright3195@gmail.com>
2019-05-05 13:07:52 -0700

    testing kube

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Home } from './pages/home/Home';
 import { SupplyPromiseClient } from './proto/supply_grpc_web_pb';
 
-const client = new SupplyPromiseClient('http://localhost:8080');
+const client = new SupplyPromiseClient('http://envoy-2rgr5:8080');
 export const ClientContext = React.createContext();
 
 function App() {
