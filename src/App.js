df9b241cfcc1bc642a390eefacd33e14867a40b4
Matt Wright <mattwright3195@gmail.com>
2019-05-05 13:29:21 -0700

    try again

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from 'react';
 import { Home } from './pages/home/Home';
 import { SupplyPromiseClient } from './proto/supply_grpc_web_pb';
 
-const client = new SupplyPromiseClient('http://34.83.209.79:30550');
+// const client = new SupplyPromiseClient('http://34.83.209.79:30550');
+const client = new SupplyPromiseClient('envoy-xwmrn:30550');
 export const ClientContext = React.createContext();
 
 function App() {
